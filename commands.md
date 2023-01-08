dd887eec310d4c48bffea285825f66b269e7ef27
devleed <mujahidwaleed11@gmail.com>
2023-01-08 12:35:29 +0500

    initial

---
+++ commands.md
@@ -0,0 +1,17 @@
+## Hardhat Commands
+
+#### Deploy
+
+npx hardhat run scripts/deploy.js --network <network>
+
+#### Verify
+
+npx hardhat verify --network <network> <address>
+
+#### Test
+
+npx hardhat test
+
+#### Compile
+
+npx hardhat compile
---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require('@nomicfoundation/hardhat-toolbox');
+
+const GOERLI_PRIVATE_KEY =
+  '72443666c98402dbea36dad599caad6a5c1987a74481fa618dae849175668c35';
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: '0.8.17',
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/80ba3747876843469bf0c36d0a355f71`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: '3MB1GI7WAG539CW1NKSHW22I7C2WH8HGB8',
+  },
+};
